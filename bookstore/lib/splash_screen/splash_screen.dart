cb32a905a7049848d7d278fca9b24214f4dd4181
Ryednap <ujjwalpandey408@gmail.com>
2021-11-03 11:28:37 +0530

    Day 2 commit

--- bookstore/lib/splash_screen/splash_screen.dart
+++ bookstore/lib/splash_screen/splash_screen.dart
@@ -1,31 +1,62 @@
+import 'package:bookstore/splash_screen/google_login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  Widget companyLogo() {
+    return Container(
+      height: 100,
+      width: 100,
+      margin: const EdgeInsets.only(top: 60.0, right: 40.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.yellowAccent.shade100.withOpacity(0.25),
+            spreadRadius: 6,
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
+        image: const DecorationImage(
+          image: AssetImage('assets/images/splash_image/Ustore.jpeg'),
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
     return Stack(
-      alignment: Alignment.topRight,
       children: <Widget>[
         SizedBox(
-          height : height,
-          width : width,
-          child : Image.asset('assets/images/splash_image/lib.jpg', fit : BoxFit.cover),
+          height: height,
+          width: width,
+          child: Image.asset(
+            'assets/images/splash_image/lib.jpg',
+            fit: BoxFit.cover,
+          ),
         ),
-        Container (
-          height : 100,
-          width : 100,
-          margin: const EdgeInsets.only(top : 30.0, right : 50),
-          child : Image.asset('assets/images/splash_image/Ustore.jpeg'),
+        Align(
+          alignment: Alignment.topRight,
+          child: companyLogo(),
         ),
-        Container (
-          child : InkWell(
-            child : 
-          )
-        )
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Row(
+              children: const <Widget>[
+                GoogleLogin(),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
